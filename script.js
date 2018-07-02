01720f82316d7537f7c91c7338ec65483fb1bd8e
Ross McCorkle <ross.mccorkle@gmail.com>
2018-07-02 15:43:10 -0700

    aria live changes

--- script.js
+++ script.js
@@ -3,23 +3,20 @@
 
 
 
-
 //stormGlass Api will return wave height and water temps for provided lat and long
 const CastUrl = "https://api.stormglass.io/forecast";
 const key = "06b36a2c-6295-11e8-8b92-0242ac120008-06b36bda-6295-11e8-8b92-0242ac120008";
 function callStormGlassApi(long, lat, Loc) {
-//console.log(long, lat)
+
 	$.ajax({
     url: CastUrl,
     data: {
     	lat: lat,
     	lng: long
-
     },
      headers: {
       'Authentication-Token': key  
     },
-
     dataType: 'json',
     type: 'GET',
     success: function(data) { 
@@ -27,10 +24,11 @@ function callStormGlassApi(long, lat, Loc) {
     	renderPage(data, Loc);
     },
     error: renderError
-    })
+  })
     	
-    }
-    
+}
+
+//getProp function determines if value returned is null (error) 
 function getProp(object, keys, defaultVal = null) {
   keys = Array.isArray(keys) ? keys : keys.replace(/(\[(\d)\])/g, '.$2').split('.');
   object = object[keys[0]];
@@ -41,22 +39,21 @@ function getProp(object, keys, defaultVal = null) {
 }
 
 function renderPage(data, Loc) {
-	//getProp function determines if value returned is null (error)
-    const height = getProp(data, 'hours[0].waveHeight[2].value')
-    
-    const water = getProp(data, 'hours[0].waterTemperature[2].value');
-    console.log(data);
-    if(height && water) {
+	//below var to capture wave height data from StormGlass API
+  const height = getProp(data, 'hours[0].waveHeight[1].value')
+  //below var to capture water temp data from StormGlass API
+  const water = getProp(data, 'hours[0].waterTemperature[2].value');
+  if(height && water) {
   	$('.output-heading').html(`<h2>Today\'s conditions for ${Loc}:</h2>`);
-	$('.output').html(`<div>Wave Height: ${height} meters  <button class="buttonOut" data-measure="${height}" data-unitsm="m">Meters/Feet</button></div>`);
-	$('.waterTemp').html(`<div>Water temp: ${water} celcius  <button class="buttonW" data-temp="${water}" data-units="c">Celcius/Fahrenheit</button></div>`);
-    } else {
-    	renderError()
-    }
-	
-	
+    $('.output-heading').removeClass('hidden-element');
+    $('.output').html(`<div>Wave Height: ${height} meters  <button class="buttonOut" data-measure="${height}" data-unitsm="m">Meters/Feet</button></div>`);
+    $('.waterTemp').html(`<div>Water temp: ${water} celcius  <button class="buttonW" data-temp="${water}" data-units="c">Celcius/Fahrenheit</button></div>`);
+  } else {
+    renderError()
+  }		
 }
- //GoogleApi translates Loc into Lat and Long coordinates
+
+//GoogleApi translates Loc into Lat and Long coordinates
 function callGoogleApi(Loc) {
 	$.ajax({
   	url: 'https://maps.googleapis.com/maps/api/geocode/json',
@@ -64,8 +61,7 @@ function callGoogleApi(Loc) {
   		key: 'AIzaSyBqCNU_iZ01XaLo3EdWRupVSr_jLhEfNqA',
   		address: Loc
   	},
-    success: function(data) {
-    	//console.log(data)
+    success: function(data) {	
     	const long = data.results[0].geometry.location.lng;
     	const lat = data.results[0].geometry.location.lat;
     	callStormGlassApi(long, lat, Loc);
@@ -73,74 +69,71 @@ function callGoogleApi(Loc) {
     error: renderError
   })
 }
+
 function renderError() {
+  $('.output-heading').empty();
+  $('.waterTemp').empty();
 	$('.output').html('<h2 class="error">No info on that location, try another one.</h2>')
 }
 
 function convert(temp, units) {
-	//logic to convert temp from Celcius to Fahrenheit
-	
+  //logic to convert temp from Celcius to Fahrenheit
 	if(units === "c") {
 		const output = Number(temp) * 9 / 5 + 32; 
-
-        const rounded = Math.round(output * 100) / 100
-		$('.waterTemp').html(`<div>Water temp: ${rounded} fahrenheit  <button class="buttonW" data-temp="${rounded}" data-units="f">C/F</button></div>`);
+    const rounded = Math.round(output * 100) / 100
+		$('.waterTemp').html(`<div>Water temp: ${rounded} fahrenheit  <button class="buttonW" data-temp="${rounded}" data-units="f">Celcius/Fahrenheit</button></div>`);
 	}
 	//logic to convert from fahrenheit to Celcius
-	else {
-		
+	else {	
 		const output = (Number(temp)-32) / 1.8
 		const rounded = Math.round(output * 100) / 100
-		$('.waterTemp').html(`<div>Water temp: ${rounded} celcius  <button class="buttonW" data-temp="${rounded}" data-units="c">C/F</button></div>`);
+		$('.waterTemp').html(`<div>Water temp: ${rounded} celcius  <button class="buttonW" data-temp="${rounded}" data-units="c">Celcius/Fahrenheit</button></div>`);
 	}
 }
+
  function LengthConverter(height, measure) {
   //upon button click, convert from meters to feet
   if(measure === "m") {
   	const output = Number(height) *3.2808;
   	//below makes decimal to second place
   	const rounded = Math.round(output * 100) / 100
-  	//console.log(output)
-  	$('.output').html(`<div>Wave Height: ${rounded} feet  <button class="buttonOut" data-measure="${rounded}" data-unitsm="f">M/F</button></div>`);
+  	
+  	$('.output').html(`<div>Wave Height: ${rounded} feet  <button class="buttonOut" data-measure="${rounded}" data-unitsm="f">Meters/Feet</button></div>`);
   }
   //feet to meters
   else {
-    
+    console.log(height)
   	const output = (Number(height) * 0.3048);
   	//below makes decimal to second place
   	const rounded = Math.round(output * 100) / 100;
-  	$('.output').html(`<div>Wave Height: ${rounded} meters  <button class="buttonOut" data-measure="${rounded}" data-unitsm="m">M/F</button></div>`);
+  	$('.output').html(`<div>Wave Height: ${rounded} meters  <button class="buttonOut" data-measure="${rounded}" data-unitsm="m">Meters/Feet</button></div>`);
   }
 }
 
-
 function watchSubmit() {
-	//capture search box value (city) upon submission and send to googleApi to get city longitude and latitude
+  //capture search box value (city) upon submission and send to googleApi to get city longitude and latitude
 	$('.js-search-form').submit(function(event) {
 		event.preventDefault();
 		const queryTarget = $('.js-query');
 		const query = queryTarget.val();
 		queryTarget.val("");
-		callGoogleApi(query);
-		
+		callGoogleApi(query);	
 	});
-	//flip water temps to Fahrenheit\Celcius
+
+	//flip water temps to Fahrenheit\Celcius on button click
 	$('.waterTemp').on('click', '.buttonW', function(event) {
 		const temp = $(this).attr('data-temp');
 		const units = $(this).attr('data-units');
-		//console.log(temp, units)
 		convert(temp, units);
 		});
+
 	//Flip wave heights feet\meters
-	
 	$('.output').on('click', '.buttonOut', function(event) {
 		const height = $(this).attr('data-measure');
 		const measure = $(this).attr('data-unitsm');
-		  //console.log(height, measure)
+    //console.log(height, measure)
 		LengthConverter(height, measure)
-
-	
-    });
-   }
+  })
+}
 
 $(watchSubmit);
